1db84fd243c124dc9db65f5c7b0067b3b1a96035
Mikołaj Maj <mikolaj.maj22@gmail.com>
2026-01-06 23:40:29 +0100

    Remove CarController and update routes to improve API structure

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/helloworld', function(){
+    
+    return ["message" => "Hello world from laravel API"];
+
+});
+
+Route::prefix('v1')->group(function(){
+
+    Route::apiResource('cars', CarController::class);
+
+});
+
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
-Route::get('/', [CarController::class, 'index']);
+Route::get('/', function(){
+
+  return view('welcome');
+
+});
