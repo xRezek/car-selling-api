69e9b644a04259fa9afb6f9e5beac39b02f0ac5f
Mikołaj Maj <mikolaj.maj22@gmail.com>
2026-01-07 23:13:52 +0100

    feat(auth): implement authentication, registration, email verification, and password reset functionality
    - Added routes for user registration, login, email verification, password reset, and logout.
    - Created controllers for handling authentication logic including LoginController, RegisteredUserController, EmailVerificationNotificationController, NewPasswordController, PasswordResetLinkController, and VerifyEmailController.
    - Implemented middleware to ensure email verification before accessing certain routes.
    - Updated tests for authentication, registration, email verification, and password reset functionalities.
    - Removed vite.config.js as part of the project restructuring.
    - Added CORS configuration to allow cross-origin requests from the frontend.
    - Refactored example tests to use Pest testing framework.

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request)
+    {
+        $request->authenticate();
+
+        $user = $request->user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return [
+
+            "user" => new UserResource($user),
+            "token" => $token
+
+        ];
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
-
-  return view('welcome');
-
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
 });
+
+
