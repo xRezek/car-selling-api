ba630ee0f3c6e5514ac7cd3d951d8de011a8085e
Mikołaj Maj <mikolaj.maj22@gmail.com>
2026-01-10 22:51:57 +0100

    feat(auth): add registration tests for valid and invalid user inputs

--- tests/Feature/Auth/RegisterApiTest.php
+++ tests/Feature/Auth/RegisterApiTest.php
@@ -1,9 +1,102 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use Pest\Support\Str;
 
-    $response->assertStatus(200);
+test('User can register with valid credentials', function(){
+
+    $response = $this->postJson('/api/register',[
+
+        'name' => "Kox",
+        'email' => 'kox@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password'
+
+    ]);
+
+    $response->assertStatus(204)
+        ->assertNoContent();
+
+
 });
 
-//todo: napisać testy do rejestracji usera
+test('User cannot register with invalid name', function(){
+
+    $response = $this->postJson('/api/register',[
+
+        'name' => Str::random(256), // 255 is maximum length
+        'email' => 'kox@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password'
+
+    ]);
+
+    $response->assertStatus(422)
+        ->assertJsonstructure([
+
+            "message",
+            "errors" => [
+
+                "name"
+
+            ]
+
+        ])
+        ->assertJsonValidationErrors(['name']);
+    
+
+});
+
+
+test('User cannot register with invalid email', function(){
+
+    $response = $this->postJson('/api/register',[
+
+        'name' => "Kox",
+        'email' => 'kox-example.com', // - instead of @
+        'password' => 'password',
+        'password_confirmation' => 'password'
+
+    ]);
+
+    $response->assertStatus(422)
+        ->assertJsonstructure([
+
+            "message",
+            "errors" => [
+
+                "email"
+
+            ]
+
+        ])
+        ->assertJsonValidationErrors(['email']);
+    
+
+});
+
+test('User cannot register with empty password',function(){
+
+    $response = $this->postJson('/api/register',[
+
+        'name' => "Kox",
+        'email' => 'kox@example.com',
+        'password' => '',
+        'password_confirmation' => ''
+
+    ]);
+
+    $response->assertStatus(422)
+    ->assertJsonstructure([
+
+        "message",
+        "errors" => [
+
+            "password"
+
+        ]
+
+    ])
+    ->assertJsonValidationErrors(['password']);
+
+
+});
